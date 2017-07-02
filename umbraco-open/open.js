9683430afafdde5a947538cf8a95c7905cf69236
Harvey Williams <harveyjwilliams@aol.co.uk>
2017-07-02 18:39:07 +0100

    Code cleanup.

--- umbraco-open/open.js
+++ umbraco-open/open.js
@@ -1,56 +1,46 @@
 browser = typeof browser === 'undefined' ? chrome : browser;
 
-const open = {};
+var open = function() {
+    // Create an anchor element to get the URL origin.
+    // http://stackoverflow.com/a/1421037
+    let getOrigin = (fullUrl) => {
+        let a = document.createElement("a");
+        a.href = fullUrl;
 
-open.name = 'UmbracoOpen';
-open.version = '0.5.0';
+        return a.origin;
+    };
 
-// Create an anchor element to get the URL origin.
-// http://stackoverflow.com/a/1421037
-open.getOrigin = (fullUrl) => {
-    let a = document.createElement("a");
-    a.href = fullUrl;
+    let toggleUmbraco = (fullUrl, index) => {
+        let origin = getOrigin(fullUrl);
 
-    return a.origin;
-};
+        browser.tabs.create({
+            url:
+                fullUrl.includes('/umbraco')
+                    ? origin // Navigate back to the homepage since we are in Umbraco.
+                    : origin + '/umbraco/', // Must have trailing slash for Umbraco 4.
+            index: index
+        });
+    };
 
-open.toggleUmbraco = (fullUrl, index) => {
-    let origin = open.getOrigin(fullUrl);
+    // Where totalTime is the time spent going around this function.
+    let clickEvent = (e, totalTime) => {
+        browser.tabs.query({ currentWindow: true, active: true }, function(tabs) {
+            if (tabs[0].status == 'loading') {
+                setTimeout(() => {
+                    clickEvent(e, (totalTime || 0) + 100);
+                }, 100);
+            } else {
+                toggleUmbraco(tabs[0].url, tabs[0].index + 1);
+            }
+        });
+    };
 
-    browser.tabs.create({
-        url:
-            fullUrl.includes('/umbraco')
-                ? origin // Navigate back to the homepage since we are in Umbraco.
-                : origin + '/umbraco/', // Must have trailing slash for Umbraco 4.
-        index: index
-    });
-};
-
-// Where totalTime is the time spent going around this function.
-open.clickEvent = (e, totalTime) => {
-    // Wait 10 seconds before giving up.
-    if (totalTime > 9999) {
-        console.log(`[${open.name}]`, 'New page took too long to load. Giving up.');
-
-        return;
-    }
-
-    browser.tabs.query({ currentWindow: true, active: true }, function(tabs) {
-        if (tabs[0].status == 'loading') {
-            setTimeout(() => {
-                open.clickEvent(e, (totalTime || 0) + 100);
-            }, 100);
-        } else {
-            open.toggleUmbraco(tabs[0].url, tabs[0].index + 1);
-        }
-    });
-};
-
-open.init = () => {
-    browser.browserAction.onClicked.addListener(open.clickEvent);
-
+    browser.browserAction.onClicked.addListener(clickEvent);
     shared.setIcon();
-};
 
-open.init();
+    return {
+        name: 'UmbracoOpen',
+        version: '0.5.0'
+    };
+}();
 
