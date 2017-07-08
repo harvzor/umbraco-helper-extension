0b3c3f8e21fc09aadd4eb886fc3e9993d6d322c5
Harvey Williams <harveyjwilliams@aol.co.uk>
2017-07-08 02:41:18 +0100

    Tidied code a little and allowed double click to open specific Umbraco node.

--- open.js
+++ open.js
@@ -2,49 +2,10 @@ browser = typeof browser === 'undefined' ? chrome : browser;
 
 var open = function() {
     let currentUrl = '';
+    let clickTimeout = null;
 
     let decruft = (text) => {
         return text.replace(")]}',", '');
-    };
-
-    let openUmbracoNode = () => {
-        let apiUrl = '/umbraco/backoffice/UmbracoApi/Entity/SearchAll?query=';
-        let path = getPath(currentUrl);
-        let alias = getAliasOfPath(path);
-        var domain = getOrigin(currentUrl);
-
-        console.log('run', domain + apiUrl + alias);
-
-        browser.cookies.get({
-            url: domain,
-            name: 'XSRF-TOKEN'
-        })
-        .then((cookie) => {
-            console.log('token', cookie.value);
-
-            let headers = new Headers({
-                'X-XSRF-TOKEN': cookie.value
-            });
-
-            fetch(domain + apiUrl + alias, {
-                method: 'GET',
-                credentials: 'same-origin',
-                headers: headers
-            })
-            .then((response) => {
-                response.text().then((text) => {
-                    var json = JSON.parse(decruft(text));
-
-                    browser.tabs.create({
-                        url: domain + '/umbraco/#/content/content/edit/' + json[0].results[0].id
-                        //index: 
-                    });
-                });
-            })
-            .catch((error) => {
-                console.log('error', error);
-            });
-        });
     };
 
     // Get an Umbraco safe alias from a path.
@@ -98,33 +59,80 @@ var open = function() {
         return a.pathname;
     };
 
-    let toggleUmbraco = (fullUrl, index) => {
-        let origin = getOrigin(fullUrl);
-
-        browser.tabs.create({
-            url:
-                fullUrl.includes('/umbraco')
-                    ? origin // Navigate back to the homepage since we are in Umbraco.
-                    : origin + '/umbraco/', // Must have trailing slash for Umbraco 4.
-            index: index
+    let createTabAfterCurrent = (url) => {
+        browser.tabs.query({ currentWindow: true, active: true }, (tabs) => {
+            browser.tabs.create({
+                url: url,
+                index: tabs[0].index + 1
+            });
         });
     };
 
-    // Where totalTime is the time spent going around this function.
-    let clickEvent = (e, totalTime) => {
-        browser.tabs.query({ currentWindow: true, active: true }, (tabs) => {
-            toggleUmbraco(currentUrl, tabs[0].index + 1);
+    let toggleUmbraco = () => {
+        let origin = getOrigin(currentUrl);
 
-            /*
-            if (tabs[0].status == 'loading') {
-                setTimeout(() => {
-                    clickEvent(e, (totalTime || 0) + 100);
-                }, 100);
-            } else {
-                toggleUmbraco(tabs[0].url, tabs[0].index + 1);
-            }
-            */
+        createTabAfterCurrent(
+            currentUrl.includes('/umbraco')
+                ? origin // Navigate back to the homepage since we are in Umbraco.
+                : origin + '/umbraco/' // Must have trailing slash for Umbraco 4.
+        );
+    };
+
+    let openUmbracoNode = () => {
+        let apiUrl = '/umbraco/backoffice/UmbracoApi/Entity/SearchAll?query=';
+        let path = getPath(currentUrl);
+        let alias = getAliasOfPath(path);
+        let domain = getOrigin(currentUrl);
+
+        browser.cookies.get({
+            url: domain,
+            name: 'XSRF-TOKEN'
+        })
+        .then((cookie) => {
+            let headers = new Headers({
+                'X-XSRF-TOKEN': cookie.value
+            });
+
+            fetch(domain + apiUrl + alias, {
+                method: 'GET',
+                credentials: 'same-origin',
+                headers: headers
+            })
+            .then((response) => {
+                response.text().then((text) => {
+                    let json = JSON.parse(decruft(text));
+
+                    createTabAfterCurrent(domain + '/umbraco/#/content/content/edit/' + json[0].results[0].id);
+                });
+            })
+            .catch((error) => {
+                console.log('error', error);
+            });
         });
+    };
+
+    // The button click event.
+    // Also tracks if the user has clicked or double clicked.
+    // - single click: toggle Umbraco
+    // - double click: open Umbraco node
+    let clickEvent = (e) => {
+        if (clickTimeout != null) {
+            clearTimeout(clickTimeout);
+
+            clickTimeout = null;
+
+            console.log('cleared', clickTimeout);
+
+            openUmbracoNode();
+        } else {
+            clickTimeout = setTimeout(() => {
+                toggleUmbraco();
+
+                clearTimeout(clickTimeout);
+
+                clickTimeout = null;
+            }, 300);
+        }
     };
 
     browser.browserAction.onClicked.addListener(clickEvent);
