f488d5769567ccffbd0174c70b315a3aaae69091
Harvey Williams <harveyjwilliams@aol.co.uk>
2017-07-09 23:16:43 +0100

    Fixed new logging function.

--- log.js
+++ log.js
@@ -3,8 +3,9 @@
 browser = typeof browser === 'undefined' ? chrome : browser;
 
 var log = function() {
+    // https://stackoverflow.com/questions/45001489/wrap-a-console-log-with-infinite-optional-params
     return function(...message) {
-        console.log(`[${open.name}]`, message);
+        console.log(`[${open.name}]`, ...message);
     };
 }();
 
