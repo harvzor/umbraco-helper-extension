e503d41f7f672ddc7b6862080666906c213dc514
Harvey <harveyjwilliams@aol.co.uk>
2019-01-09 13:08:53 +0000

    Improved logging by switching to trace.

--- scripts/log.js
+++ scripts/log.js
@@ -3,6 +3,7 @@
 var log = function() {
     let name = '';
 
+    // Bug: the name sometimes isn't found before something is logged.
     helpers.getConfig()
         .then(config => {
             name = config.name;
@@ -10,6 +11,7 @@ var log = function() {
 
     // https://stackoverflow.com/questions/45001489/wrap-a-console-log-with-infinite-optional-params
     return function(...message) {
-        console.log(`[${name}]`, ...message);
+        //console.log(`[${name}]`, ...message);
+        console.trace(`[${name}]`, ...message);
     };
 }();
